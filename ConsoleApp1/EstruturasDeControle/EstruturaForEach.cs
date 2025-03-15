f1d2f2a3623ec777fdfc98b868c06e91c9037c20
riansullivandev <rian.sullivandev@gmail.com>
2025-03-15 10:24:24 -0300

    oi

---
+++ ConsoleApp1/EstruturasDeControle/EstruturaForEach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.EstruturasDeControle
+{
+    internal class EstruturaForEach
+    {
+        public static void Executar()
+        {
+            var palavra = "opa!";
+
+            foreach (var letra in palavra)
+            {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "Ana", "Bia", "Jose" };
+            foreach (string aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
